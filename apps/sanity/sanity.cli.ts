16b3f5a0ec012db724202165bbb73736f6d33505
Bogumił <47248711+milewskibogumil@users.noreply.github.com>
2024-08-29 16:55:28 +0200

    First initialization of Astro, Sanity and Turborepo

---
+++ apps/sanity/sanity.cli.ts
@@ -0,0 +1,9 @@
+import { defineCliConfig } from 'sanity/cli'
+
+export default defineCliConfig({
+  api: {
+    projectId: 'iu4zjh9q',
+    dataset: 'production'
+  },
+  studioHost: 'kryptonum'
+})

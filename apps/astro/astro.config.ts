ad3c69ee031ab8c1d900856cecbc081a40a68c00
Bogumił <47248711+milewskibogumil@users.noreply.github.com>
2024-09-05 11:28:40 +0200

    Add astro config

---
+++ apps/astro/astro.config.ts
@@ -0,0 +1,23 @@
+import { defineConfig } from "astro/config";
+import vercel from "@astrojs/vercel/serverless";
+import sitemap from "@astrojs/sitemap";
+import { DOMAIN } from "./src/global/constants";
+import { isPreviewDeployment } from "./src/utils/is-preview-deployment";
+
+export default defineConfig({
+  site: DOMAIN,
+  integrations: [
+    sitemap(),
+  ],
+  image: {
+    remotePatterns: [{
+      protocol: "https",
+      hostname: "cdn.sanity.io"
+    }],
+  },
+  prefetch: {
+    prefetchAll: true
+  },
+  output: isPreviewDeployment ? "server" : 'hybrid',
+  adapter: vercel(),
+});

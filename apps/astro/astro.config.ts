6f6a9ba8c943c7ca71e84b53535a917cc7115c47
Bogumił <47248711+milewskibogumil@users.noreply.github.com>
2024-09-07 12:48:15 +0200

    change package.json name in astro dir and updated astro config

--- apps/astro/astro.config.ts
+++ apps/astro/astro.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "astro/config";
 import vercel from "@astrojs/vercel/serverless";
 import sitemap from "@astrojs/sitemap";
 import { DOMAIN } from "./src/global/constants";
+import { isPreviewDeployment } from "./src/utils/is-preview-deployment";
 
 export default defineConfig({
   site: DOMAIN,
@@ -17,6 +18,6 @@ export default defineConfig({
   prefetch: {
     prefetchAll: true
   },
-  output: "server",
+  output: isPreviewDeployment ? "server" : 'hybrid',
   adapter: vercel(),
 });
--- apps/astro/src/utils/is-preview-deployment.ts
+++ apps/astro/src/utils/is-preview-deployment.ts
@@ -1,1 +1,1 @@
-export const isPreviewDeployment = process.env.VERCEL_ENV === "preview" || process.env.NODE_ENV !== "production";
+export const isPreviewDeployment = import.meta.env.VERCEL_ENV === "preview" || import.meta.env.NODE_ENV !== "production";

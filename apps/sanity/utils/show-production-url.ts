37711ceecf24305ea572447f3fd623882a68ed3f
Bogumił <47248711+milewskibogumil@users.noreply.github.com>
2024-09-09 17:07:26 +0200

    update showProductionUrl util

--- apps/sanity/utils/show-production-url.ts
+++ apps/sanity/utils/show-production-url.ts
@@ -1,12 +1,11 @@
 import { definePlugin } from 'sanity'
 import { DOMAIN } from '../constants';
-import { slugList } from '../structure/slug-list';
 
 export const showProductionUrl = definePlugin({
   name: 'Production URL',
   document: {
-    productionUrl: async (_, { document: { _type } }) => {
-      const slug = slugList[_type as keyof typeof slugList];
+    productionUrl: async (_, { document }) => {
+      const slug = (document.slug as { current?: string })?.current
       if (!slug) return;
       return `${DOMAIN}${slug}`;
     }

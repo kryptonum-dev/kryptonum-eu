28d5819132e463cbf81e8bf43571ef4577d6ad4c
Bogumił <47248711+milewskibogumil@users.noreply.github.com>
2024-09-09 13:07:34 +0200

    add more explicit console.log for redirects

--- apps/astro/redirects.ts
+++ apps/astro/redirects.ts
@@ -30,8 +30,13 @@ if (isPreviewDeployment) {
       }
     ])
   );
-  console.log('\x1b[32m%s\x1b[0m', `✅ \x1b[1m${Object.keys(redirects).length}\x1b[22m redirects added from Sanity`);
-}
 
+  const permanentRedirects = data.filter(r => r.isPermanent).length;
+  const temporaryRedirects = data.length - permanentRedirects;
+  console.log(
+    '\x1b[32m%s\x1b[0m',
+    `✅ \x1b[1m${Object.keys(redirects).length}\x1b[0m\x1b[32m redirects added from Sanity (\x1b[1m${permanentRedirects}\x1b[0m\x1b[32m permanent and \x1b[1m${temporaryRedirects}\x1b[0m\x1b[32m temporary)`
+  );
+}
 
 export default redirects;

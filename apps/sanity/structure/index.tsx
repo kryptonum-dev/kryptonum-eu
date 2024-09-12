e1637034afa2d9c54037646d8f3411057cb7924a
Bogumił <47248711+milewskibogumil@users.noreply.github.com>
2024-09-12 13:02:33 +0200

    update structure files

--- apps/sanity/structure/index.tsx
+++ apps/sanity/structure/index.tsx
@@ -1,46 +1,5 @@
-import type { StructureResolver, StructureBuilder } from 'sanity/structure'
-import { Iframe, type IframeProps } from 'sanity-plugin-iframe-pane'
-import { schemaTypes, singleTypes } from "./schema-types";
-import { PREVIEW_DEPLOYMENT_DOMAIN } from '../constants';
-
-export const singletonActions = new Set(["publish", "discardChanges", "restore"]);
-export const singletonTypes = new Set(singleTypes.map(type => type.name as string));
-
-const Preview = ({ document }: { document: IframeProps['document'] }) => {
-  const slug = (document.displayed.slug as { current?: string })?.current;
-  if (!slug) return <div style={{ padding: '1rem' }}>🛑 Preview not available: The slug is missing</div>;
-  return <Iframe
-    document={document}
-    options={{
-      url: `${PREVIEW_DEPLOYMENT_DOMAIN}${slug}`,
-      reload: { button: true }
-    }} />
-}
-
-const typesToExcludePreview = ['global', 'redirects'];
-
-export const createSingleton = (S: StructureBuilder, name: string) => {
-  const { title, icon } = schemaTypes.find(item => item.name === name) as { title: string, icon: React.ReactNode };
-  return S.listItem()
-    .id(name)
-    .title(title)
-    .icon(icon)
-    .child(documentId =>
-      S.document()
-        .documentId(documentId)
-        .schemaType(name)
-        .title(title)
-        .views([
-          S.view.form().title('Editor').icon(() => '🖋️'),
-          ...(!typesToExcludePreview.includes(name) ? [
-            S.view
-              .component(Preview)
-              .title('Preview')
-              .icon(() => '👀')
-          ] : []),
-        ])
-    )
-};
+import type { StructureResolver } from 'sanity/structure'
+import { createSingleton } from './create-singleton';
 
 export const structure: StructureResolver = (S) =>
   S.list()
